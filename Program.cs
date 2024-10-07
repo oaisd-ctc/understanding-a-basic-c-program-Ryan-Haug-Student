e7577cc05331abc4411e4b3109f5ba5af6796095
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-10-07 17:57:50 +0000

    added console and wrote basic C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Ryan Haug.");
+
+        // Using Variables
+        int age = 16;
+        string greeting = "Hello World";
+        string firstname = "Ryan";
+        string lastname = "Haug";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
